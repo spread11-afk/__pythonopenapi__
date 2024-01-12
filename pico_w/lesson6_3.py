c5d691b23cd7faf559e9535c5f5c4f919dd29c39
spread11 <yyhhnnjj@gmail.com>
2024-01-12 16:22:31 +0800

    modify

---
+++ pico_w/lesson6_3.py
@@ -0,0 +1,11 @@
+from machine import Pin,Timer
+
+led25 = Pin('LED',Pin.OUT)
+
+
+def second1(t):
+    print('過1秒')
+    led25.toggle()
+    
+tim = Timer(period=1000, mode=Timer.PERIODIC, callback=second1)
+
