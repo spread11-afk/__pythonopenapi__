029ada0d54c3e74f0036eb9b0cb4630af725197a
spread11 <yyhhnnjj@gmail.com>
2024-01-19 15:43:42 +0800

    modify

--- openAPI/main.py
+++ openAPI/main.py
@@ -1,13 +1,19 @@
 from typing import Union
-
 from fastapi import FastAPI
+import redis
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+redis_conn = redis.Redis.from_url(os.environ.get('REDIS_HOST_PASSWORD'))
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    counter = redis_conn.incrby('testkey',1)
+    return {"Counter": counter}
 
 
 @app.get("/items/{item_id}")
